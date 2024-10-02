dc6b22e2dfb276bdb99b823728c617e87a8d6f5c
Patrick Nguyen <76218662+Pattypooper@users.noreply.github.com>
2024-10-02 01:57:31 -0700

    Box now renders top data properly, changed positioning

--- frontend/src/Components/Box/box.tsx
+++ frontend/src/Components/Box/box.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { TopTracks } from '../TopData/topData';
+import { useLocation } from 'react-router-dom';
+import { TopTracks, TopArtists, TopGenres } from '../TopData/topData';
 
 const Box: React.FC = () => {
+  const location = useLocation(); 
+  const pathname = location.pathname; 
+
+  const renderComponent = () => {
+    switch (pathname) {
+      case '/track/top':
+        return <TopTracks />;
+      case '/artist/top':
+        return <TopArtists />;
+      case '/genre/top':
+        return <TopGenres />;
+      default:
+        return <div>Select a category to view the stats.</div>; 
+    }
+  };
+
   return (
-    <div className="border-black border-2 p-4 w-1/4 bg-gray-100">
+    <div className="border-black border-2 p-4 w-1/4 bg-gray-100 ml-8 mt-4 drop-shadow-md">
       <h1 className="text-lg font-bold">Music Stats</h1>
-      <TopTracks />
+      {renderComponent()} 
     </div>
   );
 };
 
 export default Box;
-
